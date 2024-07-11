c89d389f022e1f3b5a228b5e5592a187eb9fccf1
AMILTONWEB <amiltonwebdesign@gmail.com>
2024-07-11 15:48:39 -0300

    Add files via upload

---
+++ Novas Paginas/Exercicio1.js
@@ -0,0 +1,18 @@
+
+        let soma1 = 10
+        let soma2 = 17
+        let totsoma1e2 = soma1+soma2
+
+        let subtracao1 = 128
+        let subtracao2 = 330
+        let totsubitracao1e2 = subitracao1 - subitracao2
+
+        function totalsoma(s1,s2) {
+            let resultado_s = s1+s2
+        }
+
+        function totalsubitracao(sbt1,sbt2){
+            let resultado_sbt = sbt1+sbt2
+
+        }
+    
